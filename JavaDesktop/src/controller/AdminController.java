5495859b0368c6d051d40f70e8494fe1137c9ba5
mchenyuxiang <csyxchen@qq.com>
2015-11-29 02:58:03 +0800

    admin can add new saler,shop,and so on

--- JavaDesktop/src/controller/AdminController.java
+++ JavaDesktop/src/controller/AdminController.java
@@ -134,32 +134,32 @@ public class AdminController {
 			jPanel1.add(new JLabel("用户名："));
 			final JTextField salerName = new JTextField(20);
 			jPanel1.add(salerName);
-			
+
 			JPanel jPanel2 = new JPanel();
 			jPanel2.add(new JLabel("电话："));
 			final JTextField salerPhone = new JTextField(20);
 			jPanel2.add(salerPhone);
-			
+
 			JPanel jPanel3 = new JPanel();
 			jPanel3.add(new JLabel("座机："));
 			final JTextField salerTel = new JTextField(20);
 			jPanel3.add(salerTel);
-			
+
 			JPanel jPanel4 = new JPanel();
 			jPanel4.add(new JLabel("QQ："));
 			final JTextField salerQQ = new JTextField(20);
 			jPanel4.add(salerQQ);
-			
+
 			JPanel jPanel5 = new JPanel();
 			jPanel5.add(new JLabel("微信："));
 			final JTextField salerWeiXin = new JTextField(20);
 			jPanel5.add(salerWeiXin);
-			
+
 			JPanel jPanel7 = new JPanel();
 			jPanel7.add(new JLabel("密码："));
 			final JTextField salerPassword = new JTextField(20);
 			jPanel7.add(salerPassword);
-			
+
 			JPanel jPanel6 = new JPanel();
 			JButton jButton = new JButton("确认");
 			jPanel6.add(jButton);
@@ -172,15 +172,17 @@ public class AdminController {
 			saleVerticalRight.add(jPanel7);
 			saleVerticalRight.add(jPanel6);
 			jf.add(saleVerticalRight, BorderLayout.CENTER);
-			
+
 			jButton.addActionListener(new ActionListener() {
-				
+
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					if(adminDao.insertSale(salerName.getText(), salerPhone.getText(), salerTel.getText()
-							, salerQQ.getText(), salerWeiXin.getText(), salerPassword.getText(), url, user, pass)){
+					if (adminDao.insertSale(salerName.getText(),
+							salerPhone.getText(), salerTel.getText(),
+							salerQQ.getText(), salerWeiXin.getText(),
+							salerPassword.getText(), url, user, pass)) {
 						JOptionPane.showMessageDialog(jf, "添加销售成功");
-					}else{
+					} else {
 						JOptionPane.showMessageDialog(jf, "添加销售失败");
 					}
 				}
@@ -207,30 +209,47 @@ public class AdminController {
 
 			JPanel jPanel1 = new JPanel();
 			jPanel1.add(new JLabel("名称："));
-			jPanel1.add(new JTextField(20));
+			final JTextField supplierName = new JTextField(20);
+			jPanel1.add(supplierName);
+
 			JPanel jPanel2 = new JPanel();
 			jPanel2.add(new JLabel("地址："));
-			jPanel2.add(new JTextField(20));
+			final JTextField supplierAddress = new JTextField(20);
+			jPanel2.add(supplierAddress);
+
 			JPanel jPanel3 = new JPanel();
 			jPanel3.add(new JLabel("联系人："));
-			jPanel3.add(new JTextField(20));
+			final JTextField supplierContact = new JTextField(20);
+			jPanel3.add(supplierContact);
+
 			JPanel jPanel4 = new JPanel();
 			jPanel4.add(new JLabel("手机："));
-			jPanel4.add(new JTextField(20));
+			final JTextField supplierPhone = new JTextField(20);
+			jPanel4.add(supplierPhone);
+
 			JPanel jPanel5 = new JPanel();
 			jPanel5.add(new JLabel("座机："));
-			jPanel5.add(new JTextField(20));
-			JPanel jPanel6 = new JPanel();
-			jPanel6.add(new JButton("确认"));
+			final JTextField supplierTel = new JTextField(20);
+			jPanel5.add(supplierTel);
+
 			JPanel jPanel7 = new JPanel();
 			jPanel7.add(new JLabel("QQ："));
-			jPanel7.add(new JTextField(20));
+			final JTextField supplierQQ = new JTextField(20);
+			jPanel7.add(supplierQQ);
+
 			JPanel jPanel8 = new JPanel();
 			jPanel8.add(new JLabel("微信："));
-			jPanel8.add(new JTextField(20));
+			final JTextField supplierWinXin = new JTextField(20);
+			jPanel8.add(supplierWinXin);
+
 			JPanel jPanel9 = new JPanel();
 			jPanel9.add(new JLabel("密码："));
-			jPanel9.add(new JTextField(20));
+			final JTextField supplierPassword = new JTextField(20);
+			jPanel9.add(supplierPassword);
+
+			JPanel jPanel6 = new JPanel();
+			JButton jButton = new JButton("确认");
+			jPanel6.add(jButton);
 
 			supplierVerticalRight.add(jPanel1);
 			supplierVerticalRight.add(jPanel2);
@@ -242,6 +261,23 @@ public class AdminController {
 			supplierVerticalRight.add(jPanel9);
 			supplierVerticalRight.add(jPanel6);
 			jf.add(supplierVerticalRight, BorderLayout.CENTER);
+
+			jButton.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (adminDao.insertSupplier(supplierName.getText(),
+							supplierAddress.getText(),
+							supplierContact.getText(), supplierPhone.getText(),
+							supplierTel.getText(), supplierQQ.getText(),
+							supplierWinXin.getText(),
+							supplierPassword.getText(), url, user, pass)) {
+						JOptionPane.showMessageDialog(jf, "添加经销商成功");
+					} else {
+						JOptionPane.showMessageDialog(jf, "添加经销商失败");
+					}
+				}
+			});
 
 		}
 
@@ -265,12 +301,17 @@ public class AdminController {
 
 			JPanel jPanel1 = new JPanel();
 			jPanel1.add(new JLabel("店铺名称："));
-			jPanel1.add(new JTextField(20));
+			final JTextField shopName = new JTextField(20);
+			jPanel1.add(shopName);
+			
 			JPanel jPanel2 = new JPanel();
 			jPanel2.add(new JLabel("店铺信息："));
-			jPanel2.add(new JTextArea(10, 20));
+			final JTextArea shopInfo = new JTextArea(10, 20);
+			jPanel2.add(shopInfo);
+
 			JPanel jPanel6 = new JPanel();
-			jPanel6.add(new JButton("确认"));
+			JButton jButton = new JButton("确认");
+			jPanel6.add(jButton);
 
 			shopVerticalRight.add(jPanel1);
 			shopVerticalRight.add(jPanel2);
@@ -278,6 +319,17 @@ public class AdminController {
 
 			jf.add(shopVerticalRight, BorderLayout.CENTER);
 
+			jButton.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (adminDao.insertShop(shopName.getText(), shopInfo.getText(), url, user, pass)) {
+						JOptionPane.showMessageDialog(jf, "添加门店成功");
+					} else {
+						JOptionPane.showMessageDialog(jf, "添加门店失败");
+					}
+				}
+			});
 		}
 
 	}
@@ -300,17 +352,34 @@ public class AdminController {
 
 			JPanel jPanel1 = new JPanel();
 			jPanel1.add(new JLabel("城市名称："));
-			jPanel1.add(new JTextField(20));
+			final JTextField departName = new JTextField(20);
+			jPanel1.add(departName);
+			
 			JPanel jPanel2 = new JPanel();
 			jPanel2.add(new JLabel("城市信息："));
-			jPanel2.add(new JTextArea(10, 20));
+			final JTextArea departInfo = new JTextArea(10, 20);
+			jPanel2.add(departInfo);
+
 			JPanel jPanel6 = new JPanel();
-			jPanel6.add(new JButton("确认"));
+			JButton jButton = new JButton("确认");
+			jPanel6.add(jButton);
 
 			departVerticalRight.add(jPanel1);
 			departVerticalRight.add(jPanel2);
 			departVerticalRight.add(jPanel6);
 			jf.add(departVerticalRight, BorderLayout.CENTER);
+			
+			jButton.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (adminDao.insertDepart(departName.getText(), departInfo.getText(), url, user, pass)) {
+						JOptionPane.showMessageDialog(jf, "添加出发地成功");
+					} else {
+						JOptionPane.showMessageDialog(jf, "添加出发地失败");
+					}
+				}
+			});
 
 		}
 
@@ -334,28 +403,45 @@ public class AdminController {
 
 			JPanel jPanel1 = new JPanel();
 			jPanel1.add(new JLabel("目的地："));
-			jPanel1.add(new JTextField(20));
+			final JTextField destinationName = new JTextField(20);
+			jPanel1.add(destinationName);
+			
 			JPanel jPanel2 = new JPanel();
 			jPanel2.add(new JLabel("目的地信息："));
-			jPanel2.add(new JTextArea(10, 20));
+			final JTextArea destinationInfo = new JTextArea(10, 20);
+			jPanel2.add(destinationInfo);
+
 			JPanel jPanel6 = new JPanel();
-			jPanel6.add(new JButton("确认"));
+			JButton jButton = new JButton("确认");
+			jPanel6.add(jButton);
 
 			destinationVerticalRight.add(jPanel1);
 			destinationVerticalRight.add(jPanel2);
 			destinationVerticalRight.add(jPanel6);
 			jf.add(destinationVerticalRight, BorderLayout.CENTER);
+			
+			jButton.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (adminDao.insertDestination(destinationName.getText(), destinationInfo.getText(), url, user, pass)) {
+						JOptionPane.showMessageDialog(jf, "添加目的地成功");
+					} else {
+						JOptionPane.showMessageDialog(jf, "添加目的地失败");
+					}
+				}
+			});
 
 		}
 
 	}
 
-	public static void main(String[] args) {
-		try {
-			new AdminController().init();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	public static void main(String[] args) {
+//		try {
+//			new AdminController().init();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
