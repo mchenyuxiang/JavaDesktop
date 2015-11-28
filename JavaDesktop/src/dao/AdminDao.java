7d31999189a2cc0dcd5b2b72b0d2d9c92d5da138
mchenyuxiang <csyxchen@qq.com>
2015-11-28 13:32:36 +0800

    add the AdminDao.java to solution the insert into the mysql

---
+++ JavaDesktop/src/dao/AdminDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class AdminDao {
+
+	// Ìí¼ÓÏúÊÛ
+	public int insertSale(String salerName, String salerPhone, String salerTel,
+			String salerQQ, String salerWeiXin, String salerPassword,
+			String url, String user, String pass) {
+		return 0;
+	}
+}
